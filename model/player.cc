cb68c80148cd1acb377aa4699a3c133ff9c3713f
Xenonstory <simon.j.hajjar@gmail.com>
2015-12-22 19:38:08 -0500

    added stub for move generation

--- model/player.cc
+++ model/player.cc
@@ -2,7 +2,28 @@
 
 Player::Player(const int level) : level(level) {
     // Sets the level of the new player
+    this->nextRow = -1;
+    this->nextCol = -1;
 }
 
 Player::~Player() {
+}
+
+void Player::generateNextMove() {
+    switch(this->level) {
+        case 0:
+            this->generateNextMove_0();
+        break;
+        default:
+            // TODO: implement AI for different levels
+            throw "Invalid AI level.";
+        break;
+    }
+}
+
+void Player::generateNextMove_0() {
+    // Ask the human to enter his moves and process them
+    // TODO: actually ask the player for moves, remove placeholder
+    this->nextRow = 1;
+    this->nextCol = 1;
 }
--- model/player.h
+++ model/player.h
@@ -5,9 +5,14 @@ class Player {
     private:
         const int level; // The level of the player: if its a human, set to 0,
                          // else set the level from 1 - 10.
+        int nextRow, nextCol; // Co-ordinates for the next move
     public:
         Player(const int); // Creates a new player
         ~Player();
+        void generateNextMove(); // Generates the next move
+
+        // Each AI will have a function for generating its moves
+        void generateNextMove_0();
 };
 
 #endif
