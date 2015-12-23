872b0d61404c1996b231d61a5cb7d241e1292a2e
Xenonstory <simon.j.hajjar@gmail.com>
2015-12-23 14:49:41 -0500

    added pawn two-step

--- model/pieces/pawn.cc
+++ model/pieces/pawn.cc
@@ -7,12 +7,20 @@ Pawn::Pawn(char type, Player * const player, int row, int col, Game * game) :
 }
 
 bool Pawn::validMove(int row, int col) const {
-    // TODO: en passent and two-step and diagonal capture
+    // TODO: en passent and diagonal capture
     int allowed = (this->player == this->game->player_1) ? -1 : 1;
     if(this->game->isEmpty(row, col)) {
         if(this->col == col && this->row + allowed == row) {
             // Standard one-step
             return true;
+        }
+
+        if(this->col == col && this->row + (2 * allowed) == row) {
+            if(this->game->isEmpty(this->row + allowed, col)
+               && this->moved == false) {
+                // Standard two-step
+                return true;
+            }
         }
     }
     return false;
