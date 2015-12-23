2ee722124ce0fde3bf359e7bfa201599faf9a135
Xenonstory <simon.j.hajjar@gmail.com>
2015-12-23 14:41:58 -0500

    simplified pawn validity

--- model/pieces/pawn.cc
+++ model/pieces/pawn.cc
@@ -8,19 +8,11 @@ Pawn::Pawn(char type, Player * const player, int row, int col, Game * game) :
 
 bool Pawn::validMove(int row, int col) const {
     // TODO: en passent and two-step and diagonal capture
-    if(this->player == this->game->player_1) {
-        // If its a white pawn
-        if(this->col == col && this->row - 1 == row) {
-            if(this->game->isEmpty(row, col)) {
-                return true;
-            }
-        }
-    } else {
-        // If its a black pawn
-        if(this->col == col && this->row + 1 == row) {
-            if(this->game->isEmpty(row, col)) {
-                return true;
-            }
+    int allowed = (this->player == this->game->player_1) ? -1 : 1;
+    if(this->game->isEmpty(row, col)) {
+        if(this->col == col && this->row + allowed == row) {
+            // Standard one-step
+            return true;
         }
     }
     return false;
