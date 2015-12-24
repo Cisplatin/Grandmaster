36a0cf5d0f162474a4c765090190db1b9778b7b9
Xenonstory <simon.j.hajjar@gmail.com>
2015-12-24 13:55:44 -0500

    cleanup

--- model/pieces/king.cc
+++ model/pieces/king.cc
@@ -7,7 +7,8 @@ King::King(char type, Player * const player, int row, int col, Game * game) :
 
 bool King::validMove(int row, int col) const {
     // Returns true if a valid relative move is given
-    // TODO: add castling
+    
+    // Checks for standard one-steps
     if(row + col == this->row + this->col - 1 ||
        row + col == this->row + this->col + 1) {
         return true;
@@ -16,5 +17,8 @@ bool King::validMove(int row, int col) const {
        (col == this->col + 1 || col == this->col - 1)) {
         return true;
     }
+
+    // TODO: castling
+
     return false;
 }
