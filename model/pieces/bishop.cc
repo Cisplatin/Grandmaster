8c95124ef4da703526990713862c28dddd50b55f
Xenonstory <simon.j.hajjar@gmail.com>
2015-12-24 13:27:33 -0500

    added bishop movement

--- model/pieces/bishop.cc
+++ model/pieces/bishop.cc
@@ -1,4 +1,5 @@
 #include "bishop.h"
+#include "../game.h"
 #include "../player.h"
 
 Bishop::Bishop(char type, Player * const player, int row, int col, Game * game) :
@@ -6,6 +7,18 @@ Bishop::Bishop(char type, Player * const player, int row, int col, Game * game) 
 }
 
 bool Bishop::validMove(int row, int col) const {
-    // TODO: Returns true if given a valid relative position                          
-    return true;
+    // Returns true if given a valid relative position                          
+    if((row - this->row == col - this->col) ||
+       (row - this->row == this->col - col)) {
+        int mini_row = (row < this->row) ? row : this->row;
+        int maxi_row = (row < this->row) ? this->row : row;
+        int mini_col = (col < this->col) ? col : this->col;
+        for(int i = 1; mini_row + i < maxi_row; i++) {
+            if(!this->game->isEmpty(mini_row + i, mini_col + i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+    return false;
 }
