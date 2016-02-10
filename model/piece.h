a7f48613c094eaeeee12eccaee8de24c440b9a88
Simon Hajjar <simon.j.hajjar@gmail.com>
2016-02-09 19:08:26 -0800

    added piece constants

--- model/piece.h
+++ model/piece.h
@@ -24,6 +24,20 @@ class Piece {
         void updateMove(int, int);
         int moved; // The number of times the piece has moved
         static Piece * generatePiece(char, Player * const, int, int, Game *);
+
+        // Constants related to the pieces
+        const static char WHITE_KING = 'K';
+        const static char BLACK_KING = 'k';
+        const static char WHITE_QUEEN = 'Q';
+        const static char BLACK_QUEEN = 'q';
+        const static char WHITE_ROOK = 'R';
+        const static char BLACK_ROOK = 'r';
+        const static char WHITE_BISHOP = 'B';
+        const static char BLACK_BISHOP = 'b';
+        const static char WHITE_KNIGHT = 'N';
+        const static char BLACK_KNIGHT = 'n';
+        const static char WHITE_PAWN = 'P';
+        const static char BLACK_PAWN = 'p';
 };
 
 #endif
