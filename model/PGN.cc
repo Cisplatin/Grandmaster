a5d35ecc00f222b7feeadd0235856a476e918e6e
Simon Hajjar <simon.j.hajjar@gmail.com>
2016-02-10 23:46:27 -0800

    added actual classes (I ALWAYS FORGET TO GIT ADD)

---
+++ model/PGN.cc
@@ -0,0 +1,11 @@
+#include "PGN.h"
+using namespace std;
+
+stack<Move *> reverseStack(stack<Move *> current) {
+    // Returns a reversed stack of the given one. This destroys
+    // the inputted stack.
+
+    // TODO actually write the function
+    stack<Move *> reversed;
+    return reversed;
+}
---
+++ model/PGN.h
@@ -0,0 +1,19 @@
+#ifndef PGN_H
+#define PGN_H
+
+// Forward decalartions of general classes
+class Move;
+
+#include <stack>
+
+/*
+    An abstract class for generating and reading PGN files
+*/
+class PGN {
+    public:
+        virtual ~PGN() = 0;
+    private:
+        static std::stack<Move *> reverseStack(std::stack<Move *>);
+};
+
+#endif
